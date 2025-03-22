d291ff20440d9f5e67a5b4707b9c6dadc44d60fb
Isi <isinsemail@gmail.com>
2025-03-22 17:06:01 +0100

    Update inference_video.py

--- inference_video.py
+++ inference_video.py
@@ -29,17 +29,17 @@ def transferAudio(sourceVideo, targetVideo):
         # create new "temp" directory
         os.makedirs("temp")
         # extract audio from video
-        os.system('ffmpeg -y -i "{}" -c:a copy -vn {}'.format(sourceVideo, tempAudioFileName))
+        os.system('ffmpeg -y -i "{}" -c:a copy -vn {} > /dev/null 2>&1'.format(sourceVideo, tempAudioFileName))
 
     targetNoAudio = os.path.splitext(targetVideo)[0] + "_noaudio" + os.path.splitext(targetVideo)[1]
     os.rename(targetVideo, targetNoAudio)
     # combine audio file and new video file
-    os.system('ffmpeg -y -i "{}" -i {} -c copy "{}"'.format(targetNoAudio, tempAudioFileName, targetVideo))
+    os.system('ffmpeg -y -i "{}" -i {} -c copy "{}" > /dev/null 2>&1'.format(targetNoAudio, tempAudioFileName, targetVideo))
 
     if os.path.getsize(targetVideo) == 0: # if ffmpeg failed to merge the video and audio together try converting the audio to aac
         tempAudioFileName = "./temp/audio.m4a"
-        os.system('ffmpeg -y -i "{}" -c:a aac -b:a 160k -vn {}'.format(sourceVideo, tempAudioFileName))
-        os.system('ffmpeg -y -i "{}" -i {} -c copy "{}"'.format(targetNoAudio, tempAudioFileName, targetVideo))
+        os.system('ffmpeg -y -i "{}" -c:a aac -b:a 160k -vn {} > /dev/null 2>&1'.format(sourceVideo, tempAudioFileName))
+        os.system('ffmpeg -y -i "{}" -i {} -c copy "{}" > /dev/null 2>&1'.format(targetNoAudio, tempAudioFileName, targetVideo))
         if (os.path.getsize(targetVideo) == 0): # if aac is not supported by selected format
             os.rename(targetNoAudio, targetVideo)
             print("Audio transfer failed. Interpolated video will have no audio")
